7ea3d813e68783bd0c06c3e3a66e84fa8dd5a6f6
Héloïse <h.cotroux@gmail.com>
2025-11-14 09:58:28 +0100

    data cleaning v2

--- visualizing.py
+++ visualizing.py
@@ -3,10 +3,11 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 
-properties_data = pd.read_csv("clean_data.csv") #change to clean data
+properties_data = pd.read_csv("clean_data_0.csv") #change to clean data
 
+print(properties_data.info())
 #properties_data.apply(pd.Series.value_counts)
-#print(properties_data.info())
+
 
 #properties_data.apply(pd.Series.describe).to_csv('describe.csv', index=False)
 
@@ -24,10 +25,11 @@ def histogram_for_nums(category : str):
 #histogram_for_nums('Subtype of property')
 #print(properties_data["Price"].corr(properties_data["Living area"]))
 
-print(properties_data.select_dtypes(include=np.number).columns)
-
+#print(properties_data.select_dtypes(include=np.number).columns)
+"""
 for column in properties_data.select_dtypes(include=np.number).columns:
     #if column != "Property ID":
     for column_comp in properties_data.select_dtypes(include=np.number).columns:
             #if column_comp != "Property ID":
         print(f"corr {column} - {column_comp} : {properties_data[column].corr(properties_data[column_comp])}")
+"""
